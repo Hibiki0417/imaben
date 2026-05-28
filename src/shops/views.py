fee4cfb3679fba4e30adc6706f4bad3d8defae36
Hibiki0417 <ashimine1184@gmail.com>
2026-05-29 04:53:26 +0900

    機能追加：弁当屋詳細ページを作成

--- src/shops/views.py
+++ src/shops/views.py
@@ -10,3 +10,26 @@ def shop_list(request):
     }
 
     return render(request, 'shops/shop_list.html', context)
+
+from django.shortcuts import render, get_object_or_404
+from .models import BentoShop
+
+
+def shop_list(request):
+    shops = BentoShop.objects.all().order_by('-updated_at')
+
+    context = {
+        'shops': shops,
+    }
+
+    return render(request, 'shops/shop_list.html', context)
+
+
+def shop_detail(request, pk):
+    shop = get_object_or_404(BentoShop, pk=pk)
+
+    context = {
+        'shop': shop,
+    }
+
+    return render(request, 'shops/shop_detail.html', context)    
