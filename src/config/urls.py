9b67524273968b8d7be30cc738a30cf7d0a0903a
Hibiki0417 <ashimine1184@gmail.com>
2026-05-29 19:01:38 +0900

    機能追加：店舗専用ログイン画面を作成

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path(
+        'manager/login/',
+        auth_views.LoginView.as_view(
+            template_name='manager/login.html'
+        ),
+        name='manager_login'
+    ),
+
+    path(
+        'manager/logout/',
+        auth_views.LogoutView.as_view(),
+        name='manager_logout'
+    ),
+
     path('shops/', include('shops.urls')),
 ]
