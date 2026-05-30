43e18d9b85b81771d8579ce1a1825a9233f7db3a
Hibiki0417 <ashimine1184@gmail.com>
2026-05-30 15:31:21 +0900

    設定：ローカル接続用の環境変数を追加

--- src/config/settings.py
+++ src/config/settings.py
@@ -137,8 +137,13 @@ ASGI_APPLICATION = 'config.asgi.application'
 
 
 
+REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
+
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [REDIS_URL],
+        },
     },
 }
